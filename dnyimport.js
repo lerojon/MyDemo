7ba896a90439a01997241694fa5d06145e1de09d
linxj38935 <linxj38935@hundsun.com>
2024-04-02 17:47:17 +0800

    dayscommit

---
+++ dnyimport.js
@@ -0,0 +1,10 @@
+export const say = function (){
+    console.log(1231)
+}
+
+export function hellow (){
+    console.log(900990)
+}
+export default function word (){
+    console.log(12313213213123)
+}
